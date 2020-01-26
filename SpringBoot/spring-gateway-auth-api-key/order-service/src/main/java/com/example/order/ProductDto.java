7d1d9ddd37d93dae9fd18e4954bb8dc6a5688c4f
gltomasz <gltomasz@gmail.com>
2020-01-26 20:14:11 +0100

    assigment-3 circuit breaker

---
+++ SpringBoot/spring-gateway-auth-api-key/order-service/src/main/java/com/example/order/ProductDto.java
@@ -0,0 +1,35 @@
+package com.example.order;
+
+public class ProductDto {
+
+    private final String name;
+    private final Integer quantity;
+    private final Money price;
+
+    public ProductDto(String name, Integer quantity, Money price) {
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public Money getPrice() {
+        return price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductDto{" +
+                "name='" + name + '\'' +
+                ", quantity=" + quantity +
+                ", price=" + price +
+                '}';
+    }
+}
--- spring-cloud-workshop/assignment-3/loan-issuance/src/main/java/com/example/loanissuance/LoanIssuanceApplication.java
+++ spring-cloud-workshop/assignment-3/loan-issuance/src/main/java/com/example/loanissuance/LoanIssuanceApplication.java
@@ -1,7 +1,15 @@
 package com.example.loanissuance;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -11,21 +19,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @SpringBootApplication
+@EnableFeignClients
 public class LoanIssuanceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoanIssuanceApplication.class, args);
 	}
-
-}
-
-
-@Configuration
-@EnableFeignClients
-class LoanConfig {
 
     @Bean
     @LoadBalanced
@@ -33,27 +38,62 @@ class LoanConfig {
         return new RestTemplate();
     }
 
+    @Bean
+    public Customizer<Resilience4JCircuitBreakerFactory> defaultCustomizer() {
+        return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+                .timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(4)).build())
+                .circuitBreakerConfig(CircuitBreakerConfig.custom()
+                        .minimumNumberOfCalls(3).build())
+                .build());
+    }
+
 }
 
 @RestController
 class LoanController {
 
+    private static final Logger log = LoggerFactory.getLogger(LoanController.class);
+
     FraudClient fraudClient;
     RestTemplate restTemplate;
+    CircuitBreakerFactory factory;
 
-    public LoanController(@LoadBalanced RestTemplate restTemplate, FraudClient fraudClient) {
+    public LoanController(@LoadBalanced RestTemplate restTemplate, FraudClient fraudClient, CircuitBreakerFactory factory) {
         this.restTemplate = restTemplate;
         this.fraudClient = fraudClient;
+        this.factory = factory;
     }
 
     @GetMapping("/resttemplate/frauds")
+    @SuppressWarnings("unchecked")
     List<String> fraudsByLb() {
-        return restTemplate.getForObject("http://fraud-detection/frauds", List.class);
+        return factory.create("resttemplate").run(()-> {
+            var frauds = restTemplate.getForObject("http://fraud-detection/frauds", List.class);
+            log.info("Frauds: " + frauds);
+            return frauds;
+        });
     }
 
     @GetMapping("/openfeign/frauds")
     List<String> fraudsByFeign() {
-        return fraudClient.frauds();
+        return factory.create("feign").run(()-> {
+            var frauds = fraudClient.frauds();
+            log.info("Frauds: " + frauds);
+            return frauds;
+        });
+    }
+
+    @GetMapping("/missing")
+    @SuppressWarnings("unchecked")
+    List<String> missing() {
+        return factory.create("missing").run(()-> {
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                log.error("Interrupted by circuit breaker");
+            }
+            return this.restTemplate.getForObject("http://fraud-detection/missing", List.class);
+        }, throwable -> Arrays.asList("fixed", "value"));
     }
 }
 
