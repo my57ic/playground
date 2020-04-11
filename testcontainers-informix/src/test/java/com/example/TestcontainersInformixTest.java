0985ab9960af9d4b492e805315a8bc3c52281e7b
gltomasz <gltomasz@gmail.com>
2020-04-11 19:18:06 +0200

    Add init script that creates test database with some data

--- testcontainers-informix/src/test/java/com/example/TestcontainersInformixTest.java
+++ testcontainers-informix/src/test/java/com/example/TestcontainersInformixTest.java
@@ -1,23 +1,23 @@
 package com.example;
 
-import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.util.TestPropertyValues;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.jdbc.JdbcTestUtils;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.MountableFile;
 
 import java.text.MessageFormat;
-import java.util.List;
-import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -32,7 +32,7 @@ class TestcontainersInformixTest {
         @Override
         public void initialize(ConfigurableApplicationContext applicationContext) {
             Integer mappedPort = ifxContainer.getMappedPort(9088);
-            String jdbcUrl = MessageFormat.format("spring.datasource.url=jdbc:informix-sqli://localhost:{0,number,#}/sysadmin:INFORMIXSERVER=informix", mappedPort);
+            String jdbcUrl = MessageFormat.format("spring.datasource.url=jdbc:informix-sqli://localhost:{0,number,#}/test:INFORMIXSERVER=informix", mappedPort);
             TestPropertyValues.of(jdbcUrl).applyTo(applicationContext.getEnvironment());
         }
     }
@@ -44,16 +44,19 @@ class TestcontainersInformixTest {
     private static GenericContainer ifxContainer = new GenericContainer<>("ibmcom/informix-developer-database:latest")
             .withExposedPorts(9088)
             .withEnv("LICENSE", "accept")
+            .withEnv("INIT_FILE", "test-schema.sql")
+            .withCopyFileToContainer(MountableFile.forClasspathResource("test-schema.sql"), "/opt/ibm/config/")
             .waitingFor(Wait.forLogMessage(".*Maximum server connections 1.*", 1));
 
+
     @Test
-    @DisplayName("Command history should not be empty")
-    void commandHistoryShouldNotBeEmpty() {
-        assertTrue(commandHistoryNotEmpty());
+    @DisplayName("Test database should be created with test data")
+    void testDatabaseShouldBeCreatedWithTestData() {
+        assertTrue(tableTestIsNotEmpty());
     }
 
-    private boolean commandHistoryNotEmpty() {
-        return !jdbcTemplate.queryForList("select * from command_history").isEmpty();
+    private boolean tableTestIsNotEmpty() {
+        return JdbcTestUtils.countRowsInTable(jdbcTemplate, "test") > 0;
     }
 
 }
---
+++ testcontainers-informix/src/test/resources/test-schema.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE test;
+
+DATABASE test;
+
+CREATE TABLE test (
+    name varchar(20),
+    description varchar(255)
+);
+
+INSERT INTO test VALUES("test_1", "test_1_description");
+INSERT INTO test VALUES("test_2", "test_2_description");
